b98ca88802ed9254a04ad14b0bc620e438ae6b09
turboska5 <andrewrnagel@gmail.com>
2016-09-28 13:44:41 -0400

    V0.1: Baseline

---
+++ src/main/java/com/andrewrnagel/ObjectiveGraderApplication.java
@@ -0,0 +1,12 @@
+package com.andrewrnagel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectiveGraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectiveGraderApplication.class, args);
+	}
+}
