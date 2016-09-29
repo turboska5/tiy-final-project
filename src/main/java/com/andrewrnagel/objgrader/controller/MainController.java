ce03d35f5599d69c436921f25971d0977a4d8aa6
Jimmy Bush <jebush94@hotmail.com>
2016-09-29 17:09:29 -0400

    Added html pages and some controller defaults

--- src/main/java/com/andrewrnagel/objgrader/controller/MainController.java
+++ src/main/java/com/andrewrnagel/objgrader/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.andrewrnagel.objgrader.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Andrew Nagel on 9/28/16 at 2:47 PM EST.
@@ -8,4 +10,19 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainController {
+    @RequestMapping(value = "/ManageClass", method = RequestMethod.POST)
+    public String classForm() {
+
+        return "adminUser";
+    }
+    @RequestMapping(value = "/ManageTeacher", method = RequestMethod.POST)
+    public String teacherForm() {
+
+        return "adminUser";
+    }
+    @RequestMapping(value = "/ManageStudent", method = RequestMethod.POST)
+    public String studentForm() {
+
+        return "adminUser";
+    }
 }
