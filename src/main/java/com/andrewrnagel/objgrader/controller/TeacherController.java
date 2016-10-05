fffd586e7f1649c36d25b8143959c21ef6b2b5d1
turboska5 <andrewrnagel@gmail.com>
2016-10-05 16:20:17 -0400

    days end

--- src/main/java/com/andrewrnagel/objgrader/controller/TeacherController.java
+++ src/main/java/com/andrewrnagel/objgrader/controller/TeacherController.java
@@ -66,7 +66,7 @@ public class TeacherController {
 //            Assignment assignment = mainService.getAssignment(assignmentID);
 //            model.addAttribute("assignment", assignment);
         } else {
-            Assignment assignment = new Assignment("", "", "", 0);
+            Assignment assignment = new Assignment("", "", "2016-09-01", 0, 0);
             model.addAttribute("assignment", assignment);
         }
         return "teacherManageAssign";
