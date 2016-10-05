7b7860f6e0a53d87dc3ad6903209706f05a7f9ff
turboska5 <andrewrnagel@gmail.com>
2016-10-05 11:58:07 -0400

    more fun

--- src/main/java/com/andrewrnagel/objgrader/controller/AdminController.java
+++ src/main/java/com/andrewrnagel/objgrader/controller/AdminController.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.sql.SQLException;
