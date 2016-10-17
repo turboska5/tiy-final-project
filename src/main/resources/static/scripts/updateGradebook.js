5f0f4e037d443fd8d9bd66f4e7b58096470eec2c
turboska5 <andrewrnagel@gmail.com>
2016-10-17 16:11:06 -0400

    progress

--- src/main/resources/static/scripts/updateGradebook.js
+++ src/main/resources/static/scripts/updateGradebook.js
@@ -1,14 +1,70 @@
 $(function() {
-    $.get("/classTable", function(data) {
-        $("#classOutput").empty();
-        $("#classOutput").append(data);
-    })
-    $.get("/assignTable", function(data) {
-        $("#assignmentOutput").empty();
-        $("#assignmentOutput").append(data);
-    })
-    $.get("/studentTable", function(data) {
-        $("#studentOutput").empty();
-        $("#studentOutput").append(data);
-    })
+    //preload data
+    classSearch("/classTable");
+    assignSearch("/assignTable");
+    studentSearch("/studentTable");
+
+    //button functions
+    $("#classSearch").click(function(){
+        var query = "/classTable?period=" + $("#period").val() + "&name=" + $("#name").val() + "&identifier=" + $("#identifier").val();
+        classSearch(query);
+        return false; // this prevents the form from being submitted when the button is clicked.
+    });
+    $("#classReset").click(function(){
+        $("#period").val("");
+        $("#name").val("");
+        $("#identifier").val("");
+        classSearch("/classTable");
+        return false; // this prevents the form from being submitted when the button is clicked.
+    });
+    $("#assignSearch").click(function(){
+        var query = "/assignTable?aPeriod=" + $("#aPeriod").val() + "&aName=" + $("#aName").val() + "&aID=" + $("#aID").val()
+            + "&aDate=" + $("#aDate").val() + "&aPoints=" + $("#aPoints").val();
+        assignSearch(query);
+        return false; // this prevents the form from being submitted when the button is clicked.
+    });
+    $("#assignReset").click(function(){
+        $("#aPeriod").val("");
+        $("#aName").val("");
+        $("#aID").val("");
+        $("#aDate").val("");
+        $("#aPoints").val("");
+        assignSearch("/assignTable");
+        return false; // this prevents the form from being submitted when the button is clicked.
+    });
+    $("#studentSearch").click(function(){
+        var query = "/studentTable?sPeriod=" + $("#sPeriod").val() + "&sLastName=" + $("#sLastName").val() + "&sFirstName=" + $("#sFirstName").val()
+            + "&sAName=" + $("#sAName").val() + "&sAID=" + $("#sAID").val();
+        studentSearch(query);
+        return false; // this prevents the form from being submitted when the button is clicked.
+    });
+    $("#studentReset").click(function(){
+        $("#sPeriod").val("");
+        $("#sLastName").val("");
+        $("#sFirstName").val("");
+        $("#sAName").val("");
+        $("#sAID").val("");
+        studentSearch("/studentTable");
+        return false; // this prevents the form from being submitted when the button is clicked.
+    });
+
+    //supporting functions
+    function classSearch(classQuery) {
+        $.get(classQuery, function(data) {
+            $("#classOutput").empty();
+            $("#classOutput").append(data);
+        })
+    }
+    function assignSearch(assignQuery) {
+        $.get(assignQuery, function(data) {
+            $("#assignmentOutput").empty();
+            $("#assignmentOutput").append(data);
+        })
+    }
+    function studentSearch(studentQuery) {
+        $.get(studentQuery, function(data) {
+            $("#studentOutput").empty();
+            $("#studentOutput").append(data);
+        })
+    }
 })
