e2f5a1badb349a928f29635abf9d8e8f0da71025
turboska5 <andrewrnagel@gmail.com>
2016-10-19 09:27:53 -0400

    data ordering

--- src/main/java/com/andrewrnagel/objgrader/repository/AdminRepository.java
+++ src/main/java/com/andrewrnagel/objgrader/repository/AdminRepository.java
@@ -19,6 +19,8 @@ public interface AdminRepository extends JpaRepository<Admin, Integer> {
     @Query(value = "SELECT COUNT(a.adminID) FROM Admin AS a WHERE (?1 = '' OR upper(a.lastName) LIKE upper(?1)) AND (?2 = '' OR upper(a.firstName) LIKE upper(?2)) AND (?3 = '' OR upper(a.user.email) LIKE upper(?3)) AND (?4 = '' OR upper(a.title) LIKE upper(?4))")
     Integer findAllWithoutPages(String lastName, String firstName, String email, String title);
 
-    @Query(value = "SELECT a FROM Admin a WHERE (?1 = '' OR upper(a.lastName) LIKE upper(?1)) AND (?2 = '' OR upper(a.firstName) LIKE upper(?2)) AND (?3 = '' OR upper(a.user.email) LIKE upper(?3)) AND (?4 = '' OR upper(a.title) LIKE upper(?4)) ORDER BY a.lastName ASC")
+    @Query(value = "SELECT a FROM Admin a WHERE (?1 = '' OR upper(a.lastName) LIKE upper(?1)) AND (?2 = '' OR upper(a.firstName) LIKE upper(?2)) AND (?3 = '' OR upper(a.user.email) LIKE upper(?3)) AND (?4 = '' OR upper(a.title) LIKE upper(?4)) " +
+            "ORDER BY CASE WHEN a.title = 'Principal' THEN '1'" +
+            "WHEN a.title = 'Asst. Principal' THEN '2' ELSE a.title END, a.lastName ASC")
     Page<Admin> findAllWithPages(String lastName, String firstName, String email, String title, Pageable pageable);
 }
